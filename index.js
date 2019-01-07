4608c0c120bf166bbdf1399a41d02a0741e06395
recidvst <cdsnowden@gmail.com>
2019-01-07 23:41:32 +0000

    tweak as package

--- index.js
+++ index.js
@@ -7,23 +7,23 @@
  * @return {function}
  */
 
-module.exports = function(ghosts) {
+module.exports = function (ghosts) {
     ghosts = ghosts || [];
 
-    if ( !Array.isArray(ghosts) ) return function(req, res, next) { 
+    if (!Array.isArray(ghosts)) return function (req, res, next) {
         res.setHeader('X-Clacks-Overhead', 'GNU Terry Pratchett');
         next()
     }
     else {
         ghosts.unshift('GNU Terry Pratchett');
-        ghosts = ghosts.join(', ').toString(); 
+        ghosts = ghosts.join(', ').toString();
 
-        return function(req, res, next) {    
-            if ( ghosts.length > 0 ) {        
+        return function (req, res, next) {
+            if (ghosts.length > 0) {
                 res.setHeader('X-Clacks-Overhead', ghosts);
-            }    
-            next();    
+            }
+            next();
         }
-    }   
-       
+    }
+
 };
